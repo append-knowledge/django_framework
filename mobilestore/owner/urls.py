9cb5c4f069209c734211775f8da9a3181d2d062e
LIJO JOSE <87125413+append-knowledge@users.noreply.github.com>
2021-09-22 10:37:29 +0530

    Delete mobilestore directory

--- mobilestore/owner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from owner import views
-urlpatterns=[
-    path("",views.signup,name="signup"),
-    path('signin',views.signin,name="signin"),
-    path('mobile/add',views.addmobile,name="addmobile")
-]
